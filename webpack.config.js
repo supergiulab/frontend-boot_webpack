c7c61e045ebf2e45cd606435f08c0e429f373529
supergiulab <me@supergiulab.com>
2019-10-13 13:34:22 +0200

    add css-loader post-css sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,31 @@ module.exports = {
 						presets: ['@babel/preset-env']
 					}
 				}
+			},
+			{
+				// Apply rule for .sass, .scss or .css files
+				test: /\.(sa|sc|c)ss$/,
+
+				// Set loaders to transform files.
+				// Loaders are applying from right to left(!)
+				// The first loader will be applied after others
+				use: [
+					{
+						// This loader resolves url() and @imports inside CSS
+						loader: "css-loader",
+					},
+					{
+						// Then we apply postCSS fixes like autoprefixer and minifying
+						loader: "postcss-loader"
+					},
+					{
+						// First we transform SASS to standard CSS
+						loader: "sass-loader"
+						options: {
+							implementation: require("sass")
+						}
+					}
+				]
 			}
 		]
 	},
